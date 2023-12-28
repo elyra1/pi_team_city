559fdd3822bf90ee0bb75711257cd207d1b1d502
elyra1 <afonchenkov98@bk.ru>
2023-12-28 19:26:37 +0300

    fix + tests added

--- programm.py
+++ programm.py
@@ -1,14 +1,18 @@
+import unittest
 from math import sqrt
 
-
 def find_hypotenuse(a,b):
-    return sqrt(a**2 + b**2
+    if (a == 0 or b == 0):
+        raise Exception('A side cannot be equal to 0')
+    return sqrt(a**2 + b**2)
 
 
-a = 4
-b = 3
+if __name__ == "__main__":
+    a = 4
+    b = 3
+    h = find_hypotenuse(a, b)
+    print(f'Find hypotenuse')
+    print(f'a:{a}, b:{b}')
+    print(f"Hypotenuse {h}")
 
-h = find_hypotenuse(a, b)
-print(f'Find hypotenuse')
-print(f'a:{a}, b:{b}')
-print(f"Hypotenuse {h}")
+
---
+++ test.py
@@ -0,0 +1,22 @@
+from programm import find_hypotenuse
+def test_find_hypotenuse_default():
+    assert(5 == find_hypotenuse(3, 4))
+
+def test_find_hypotenuse_0_side():
+    try:
+        find_hypotenuse(0, 4)
+        assert(False)
+    except:
+        assert(True)
+
+def test_find_hypotenuse_correct():
+    h = find_hypotenuse(5, 4)
+    assert(h**2 - 25 == 16)
+    assert(h**2 - 16 == 25)
+    
+test_find_hypotenuse_default()
+test_find_hypotenuse_0_side()
+test_find_hypotenuse_correct()
+print("Tests completed")
+
+
